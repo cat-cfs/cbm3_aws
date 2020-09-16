a300682f9cdffdae6122a430afb915bbbd05a5dc
Scott Morken <scottm361@gmail.com>
2020-09-16 15:50:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# nir_aws
+
+Scripts and commands for deploying and running NIR Uncertainty analysis on AWS
+
+## Instance Requirements
+
+* The [CBM-CFS3 toolbox](https://www.nrcan.gc.ca/climate-change/impacts-adaptations/climate-change-impacts-forests/carbon-accounting/carbon-budget-model/13107)
+* The Windows operating system
+* python 3x
+* [git](https://git-scm.com/) 
+* [python packages](https://github.com/cat-cfs/cbm3_python/blob/master/requirements.txt)
+
+
+
+## Installing
+
+The scripts can be directly installed from github
+
+```bash
+pip install git+https://github.com/smorken/nir_aws.git
+```
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+console_scripts = [
+    "nir_aws_instance = nir_aws.scripts.run_instance:main"
+]
+
+setup(
+    name="nir_aws",
+    version="0.1.0",
+    description="Scripts for Deploying NIR uncertainty analysis on AWS",
+    keywords=["cbm-cfs3", "AWS"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="",
+    download_url="",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": console_scripts
+    },
+    install_requires=requirements
+)

a0611c4b53a5301fd8020b4e5bd60205238d483d
Scott Morken <scottm361@gmail.com>
2020-09-23 13:40:40 -0700

    change state machine defintions to python scripts

---
+++ cbm3_aws/local/cbm3_run_state_machine.py
@@ -0,0 +1,25 @@
+
+def get_state_machine(task_state_machine_arn, max_concurrency):
+    return {
+        "Comment": "Task to launch CBM run task state machines",
+        "StartAt": "map_tasks",
+        "States": {
+            "map_tasks": {
+                "Type": "Map",
+                "InputPath": "$.jobs",
+                "ItemsPath": "$.jobs",
+                "MaxConcurrency": max_concurrency,
+                "Iterator": {
+                    "StartAt": "launch_cbm_task",
+                    "States": {
+                        "launch_cbm_task": {
+                            "Type": "Task",
+                            "Resource": task_state_machine_arn,
+                            "End": True
+                        }
+                    }
+                },
+                "End": True
+            }
+        }
+    }
--- cbm3_aws/local/cbm3_run_task_state_machine.py
+++ cbm3_aws/local/cbm3_run_task_state_machine.py
@@ -1,27 +1,30 @@
-{
-    "Comment": "Task to launch CBM runs and retry if they experience a spot interruption",
-    "StartAt": "RunCBMTask",
-    "States": {
-        "RunCBMTask": {
-            "Comment": "starts the CBM run task activity",
-            "Type": "activity",
-            "Resource": "{{cbm_run_task_activity_name}}",
-            "HeartbeatSeconds": 60,
-            "Retry": [
-                {
-                    "ErrorEquals": [ "States.Timeout" ],
-                    "Next": "DelayThenRestart"
-                },
-                {
-                    "ErrorEquals": [ "States.ALL" ],
-                    "End": true
-                }
-            ]
-        },
-        "DelayThenRestart": {
-            "Type": "Wait",
-            "Seconds": 10,
-            "Next": "RunCBMTask"
+
+def get_state_machine(cbm_run_task_activity_name):
+    return {
+        "Comment": "Task to launch CBM runs and retry if they experience a "
+                   "spot interruption",
+        "StartAt": "RunCBMTask",
+        "States": {
+            "RunCBMTask": {
+                "Comment": "starts the CBM run task activity",
+                "Type": "activity",
+                "Resource": cbm_run_task_activity_name,
+                "HeartbeatSeconds": 60,
+                "Retry": [
+                    {
+                        "ErrorEquals": ["States.Timeout"],
+                        "Next": "DelayThenRestart"
+                    },
+                    {
+                        "ErrorEquals": ["States.ALL"],
+                        "End": True
+                    }
+                ]
+            },
+            "DelayThenRestart": {
+                "Type": "Wait",
+                "Seconds": 10,
+                "Next": "RunCBMTask"
+            }
         }
     }
-}

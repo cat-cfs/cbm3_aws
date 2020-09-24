16b0b2d0884face69234674a97bc43d0759406b9
Scott Morken <scottm361@gmail.com>
2020-09-24 15:55:44 -0700

    working out IAM roles

--- cbm3_aws/local/roles.py
+++ cbm3_aws/local/roles.py
@@ -1,4 +1,5 @@
 from cbm3_aws import constants
+import json
 
 
 def create_state_machine_execution_policy(account_number):
@@ -34,3 +35,55 @@ def create_state_machine_execution_policy(account_number):
             }
         ]
     }
+
+
+def create_instance_iam_role(client, s3_bucket_name):
+    policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "0",
+                "Effect": "Allow",
+                "Action": [
+                    "s3:PutObject",
+                    "s3:GetObject",
+                    "s3:DeleteObject"
+                ],
+                "Resource": f"arn:aws:s3:::{s3_bucket_name}/*"
+            }
+        ]
+    }
+
+    create_policy_response = client.create_policy(
+        PolicyName='cbm3_s3_instance_policy',
+        Path='/',
+        PolicyDocument=json.dumps(policy),
+        Description='grants basic access to a particular s3 bucket for '
+                    'IAM instance role')
+
+    ec2_assume_role_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ec2.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+
+    client.create_role(
+        Path='/',
+        RoleName='cbm3_instance_iam_role',
+        AssumeRolePolicyDocument=json.dumps(ec2_assume_role_policy),
+        Description='grants ec2 instances read and write permission to '
+                    'specific bucket',
+        PermissionsBoundary='string',
+        Tags=[
+            {
+                'Key': 'string',
+                'Value': 'string'
+            }
+        ])

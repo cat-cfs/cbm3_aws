9ae39c41b45e958c840aeccafc34396214229a0c
Scott Morken <scottm361@gmail.com>
2020-09-24 10:53:42 -0700

    launch template

--- cbm3_aws/constants.py
+++ cbm3_aws/constants.py
@@ -1,2 +1,5 @@
 
 CBM_RUN_ACTIVITY_NAME = "cbm3_run_activity"
+
+AUTOSCALE_LAUNCH_TEMPLATE_NAME = "cbm3_run_launch_template"
+AUTOSCALE_IAM_INSTANCE_PROFILE_NAME = "cbm3_run_s3_access"
---
+++ cbm3_aws/local/autoscale_group.py
@@ -0,0 +1,83 @@
+import uuid
+import datetime
+from cbm3_aws import constants
+
+
+def create_launch_template(client, image_ami_id, instance_type,
+                           iam_instance_profile_arn):
+    """Create a launch template for provisioning instances
+
+    Args:
+        client (EC2.Client): boto3 ec2 client
+        image_ami_id (str): the ami id for the launched instances
+        instance_type (str): the type of the instance to launch
+            (ex. 't1.micro')
+        iam_instance_profile_arn (str): ARN for for the Iam instance profile
+            to attach to launched instances
+
+    Returns:
+        dict: the return value of boto3.EC2.Client.create_launch_template
+    """
+    client_token = str(uuid.uuid4())
+    spot_request_valid_date = \
+        datetime.datetime.today() + datetime.timedelta(days=7)
+    response = client.create_launch_template(
+        DryRun=False,
+        ClientToken=client_token,
+        LaunchTemplateName=constants.AUTOSCALE_LAUNCH_TEMPLATE_NAME,
+        LaunchTemplateData={
+            'EbsOptimized': False,
+            'IamInstanceProfile': {
+                'Arn': iam_instance_profile_arn,
+                'Name': constants.AUTOSCALE_IAM_INSTANCE_PROFILE_NAME
+            },
+            'ImageId': image_ami_id,
+            'InstanceType': instance_type,
+            'Monitoring': {
+                'Enabled': True
+            },
+            'InstanceInitiatedShutdownBehavior': 'terminate',
+            'UserData': 'string',
+            'TagSpecifications': [
+                {
+                    'ResourceType': 'instance',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': 'CBM3 Worker Instance'
+                        },
+                    ]
+                },
+                {
+                    'ResourceType': 'volume',
+                    'Tags': [
+                        {
+                            'Key': 'Name',
+                            'Value': 'CBM3 Worker volume'
+                        },
+                    ]
+                },
+            ],
+            'InstanceMarketOptions': {
+                'MarketType': 'spot',
+                'SpotOptions': {
+                    # 'MaxPrice': 'string', # up to the default on-demand price
+                    'SpotInstanceType': 'one-time',
+                    'ValidUntil': spot_request_valid_date,
+                    'InstanceInterruptionBehavior': 'terminate'
+                }
+            }
+        },
+        TagSpecifications=[
+            {
+                'ResourceType': 'launch-template',
+                'Tags': [
+                    {
+                        'Key': 'name',
+                        'Value': 'CBM3 launch template'
+                    },
+                ]
+            },
+        ]
+    )
+    return response

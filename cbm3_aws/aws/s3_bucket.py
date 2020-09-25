c47714c2aeabadf1b6586ea40bbc0b49626323c9
Scott Morken <scottm361@gmail.com>
2020-09-25 05:28:25 -0700

    add function to create s3 bucket

---
+++ cbm3_aws/aws/s3_bucket.py
@@ -0,0 +1,15 @@
+
+def create_bucket(client, bucket_name, region):
+    """Create an S3 bucket in a specified region
+
+    If a region is not specified, the bucket is created in the S3 default
+    region (us-east-1).
+
+    Args:
+        bucket_name (str): Bucket to create
+        region (str): String region to create bucket in, e.g., 'us-west-2'.
+    """
+    location = {'LocationConstraint': region}
+    client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration=location)

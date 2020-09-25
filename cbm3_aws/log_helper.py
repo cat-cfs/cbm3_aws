0605a65a1874d2a47e3c87b5a7c3b8e58e0ebcf9
Scott Morken <scottm361@gmail.com>
2020-09-25 13:38:49 -0700

    add logging helper

---
+++ cbm3_aws/log_helper.py
@@ -0,0 +1,26 @@
+import datetime
+import logging
+
+
+def start_logging(level):
+    rootLogger = logging.getLogger()
+
+    logFormatter = logging.Formatter(
+        "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+        datefmt="%m-%d %H:%M")
+
+    log_path = "spatial_rollback_{date}.log".format(
+        date=datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
+    fileHandler = logging.FileHandler(log_path, "w")
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+
+    rootLogger.setLevel(level)
+
+
+def get_logger():
+    return logging.getLogger("cbm3_aws")

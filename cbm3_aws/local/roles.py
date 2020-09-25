74eaea8260d4254e4d1b74cd2149526588414a36
Scott Morken <scottm361@gmail.com>
2020-09-25 04:34:18 -0700

    add role creation for state machine

--- cbm3_aws/local/roles.py
+++ cbm3_aws/local/roles.py
@@ -1,8 +1,16 @@
+from types import SimpleNamespace
 from cbm3_aws import constants
 import json
 
 
-def create_state_machine_execution_policy(account_number):
+def delete_role(client, role_context):
+    """delete the role and policies based on the name/ARN specified in
+     role_context"""
+    pass
+
+
+def create_state_machine_execution_policy(client, account_number):
+    role_name = "cbm3_state_machine_role"
     prefix = f"arn:aws:states:*:{account_number}"
     resource_arn_list = [
         f"{prefix}:activity:{constants.CBM_RUN_ACTIVITY_NAME}",
@@ -36,8 +44,43 @@ def create_state_machine_execution_policy(account_number):
         ]
     }
 
+    states_assume_role_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "states.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+
+    create_policy_response = client.create_policy(
+        PolicyName='cbm3_state_machine_policy',
+        Path='/',
+        PolicyDocument=json.dumps(policy),
+        Description='grants access for state machine execution')
+
+    create_role_response = client.create_role(
+        Path='/',
+        RoleName=role_name,
+        AssumeRolePolicyDocument=json.dumps(states_assume_role_policy),
+        Description='grants ec2 instances read and write permission to '
+                    'specific bucket')
+
+    client.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn=create_policy_response["Arn"])
+
+    return SimpleNamespace(
+        role_name=create_role_response["RoleName"],
+        policy_arn=create_policy_response["Arn"])
+
 
 def create_instance_iam_role(client, s3_bucket_name):
+    role_name = "cbm3_instance_iam_role"
     policy = {
         "Version": "2012-10-17",
         "Statement": [
@@ -74,16 +117,17 @@ def create_instance_iam_role(client, s3_bucket_name):
         ]
     }
 
-    client.create_role(
+    create_role_response = client.create_role(
         Path='/',
-        RoleName='cbm3_instance_iam_role',
+        RoleName=role_name,
         AssumeRolePolicyDocument=json.dumps(ec2_assume_role_policy),
         Description='grants ec2 instances read and write permission to '
-                    'specific bucket',
-        PermissionsBoundary='string',
-        Tags=[
-            {
-                'Key': 'string',
-                'Value': 'string'
-            }
-        ])
+                    'specific bucket')
+
+    client.attach_role_policy(
+        RoleName=role_name,
+        PolicyArn=create_policy_response["Arn"])
+
+    return SimpleNamespace(
+        role_name=create_role_response["RoleName"],
+        policy_arn=create_policy_response["Arn"])

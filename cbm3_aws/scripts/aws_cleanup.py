acbfff00b5a500f48480c61f2ad7982ff7cd3179
Scott Morken <scottm361@gmail.com>
2020-09-28 16:52:29 -0700

    add cleanup method

---
+++ cbm3_aws/scripts/aws_cleanup.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+from argparse import ArgumentParser
+from cbm3_aws import log_helper
+from cbm3_aws.aws import resources
+from cbm3_aws.namespace import Namespace
+
+
+def main():
+    parser = ArgumentParser(
+        description="Deallocates AWS resources that were allocated for "
+                    "cbm3_aws runs")
+
+    parser.add_argument(
+        "--resource_description_path", required=True,
+        help="Path to a json formatted file containing the allocated AWS "
+             "resources to de-allocate with this script.")
+
+    args = parser.parse_args()
+
+    log_helper.start_logging(level="DEBUG")
+    logger = log_helper.get_logger()
+    logger.info("aws_cleanup start up")
+    logger.info(vars(args))
+
+    path = os.path.abspath(args.resource_description_path)
+    with open(path, 'r') as fp:
+        data = Namespace(**json.load(fp))
+    resources.cleanup(resource_description=data)
+
+
+if __name__ == "__main__":
+    main()

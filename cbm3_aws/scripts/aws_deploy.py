cea159a2cec4715d043427c444265d352a9864c4
Scott Morken <scottm361@gmail.com>
2020-09-28 14:38:40 -0700

    implement allocation cli

--- cbm3_aws/scripts/aws_deploy.py
+++ cbm3_aws/scripts/aws_deploy.py
@@ -1,13 +1,51 @@
+import os
 from argparse import ArgumentParser
 from cbm3_aws import log_helper
-from cbm3_aws.aws import app
+from cbm3_aws.aws import resources
 
 
 def main():
-    log_helper.start_logging()
+
+    parser = ArgumentParser(
+        description="Deploy a cbm3_aws cluster for processing CBM3 runs")
+
+    parser.add_argument(
+        "--region_name", required=True,
+        help="The region name for deploying the cluster")
+    parser.add_argument(
+        "--s3_bucket_name", required=True,
+        help="Name of the s3 bucket that the the application will interact"
+             "with")
+    parser.add_argument(
+        "--min_instances", required=True,
+        help="minimum number of instances to deploy")
+    parser.add_argument(
+        "--max_instances", required=True,
+        help="maximum number of instances to deploy")
+    parser.add_argument(
+        "--image_ami_id", required=True,
+        help="Amazon machine image id for launching instances")
+    parser.add_argument(
+        "--instance_type", required=True,
+        help="the Amazon EC2 instance type to use (eg: 'a1.medium'")
+    parser.add_argument(
+        "--resource_description_dir",
+        help="path to a writeable dir for recording information for the "
+             "allocated AWS resources", default=".")
+
+    args = parser.parse_args()
+
+    log_helper.start_logging(level="DEBUG")
     logger = log_helper.get_logger()
     logger.info("aws_deploy start up")
+    logger.info(vars(args))
 
-    app.start(
-        region_name, s3_bucket_name, n_instances, image_ami_id,
-        instance_type)
+    resources.deploy(
+        region_name=args.region_name,
+        s3_bucket_name=args.s3_bucket_name,
+        min_instances=args.min_instances,
+        max_instances=args.max_instances,
+        image_ami_id=args.image_ami_id,
+        instance_type=args.instance_type,
+        resource_description_out_dir=os.path.abspath(
+            args.resource_description_dir))

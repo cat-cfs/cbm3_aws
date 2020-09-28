f06715e6d58a22a6b8a999a5effb6f753094c707
Scott Morken <scottm361@gmail.com>
2020-09-28 14:36:35 -0700

    seperate uuid (so it can be used to name other things)

--- cbm3_aws/aws/names.py
+++ cbm3_aws/aws/names.py
@@ -1,10 +1,13 @@
-from types import SimpleNamespace
+from cbm3_aws.namespace import Namespace
 import shortuuid
 
 
-def get_names():
-    uuid = shortuuid.uuid()
-    return SimpleNamespace(
+def get_uuid():
+    return shortuuid.uuid()
+
+
+def get_names(uuid):
+    return Namespace(
         run_activity=f"cbm3_run_activity_{uuid}",
         autoscale_launch_template=f"cbm3_run_launch_template_{uuid}",
         autoscale_group=f"cbm3_autoscale_group_{uuid}",
@@ -12,7 +15,6 @@ def get_names():
         run_state_machine=f"cbm3_run_state_machine_{uuid}")
 
 
-def get_step_functions_executions_name():
-    uuid = shortuuid.uuid()
-    return SimpleNamespace(
+def get_step_functions_executions_name(uuid):
+    return Namespace(
         step_functions_execution=f"cbm3_aws_step_functions_execution_{uuid}")

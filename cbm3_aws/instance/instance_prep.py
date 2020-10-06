1b09134b829718f6e798c40bb6b818b0617b26ee
Scott Morken <scottm361@gmail.com>
2020-10-06 16:59:43 -0700

    more commands for startup

--- cbm3_aws/instance/instance_prep.py
+++ cbm3_aws/instance/instance_prep.py
@@ -18,12 +18,19 @@ SOFTWARE_LIST = [
         "file_name": "AccessDatabaseEngine_X64.exe",
         "install_command": ["AccessDatabaseEngine_X64.exe", "/quiet"]
     }
+
 ]
 
 instance_commands = [
     "Read-S3Object -BucketName cat-cfs -Key instance_prep/software.zip ~\software.zip",
     "Expand-Archive -LiteralPath ~\software.zip -DestinationPath ~\software",
-    'Start-Process -FilePath ~\software\python-3.9.0-amd64.exe -ArgumentList "/quiet TargetDir=c:\python InstallAllUsers=1 PrependPath=1 Include_test=0" -NoNewWindow -Wait'
+    'Start-Process -FilePath ~\software\python-3.9.0-amd64.exe -ArgumentList "/quiet TargetDir=c:\python InstallAllUsers=1 PrependPath=1 Include_test=0" -NoNewWindow -Wait',
+    'Start-Process -FilePath C:\users\Administrator\software\AccessDatabaseEngine_X64.exe -ArgumentList "/quiet" -NoNewWindow -Wait',
+    '[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\python")',
+    '[Environment]::SetEnvironmentVariable("Path", "$env:Path;C:\python\scripts")'
+    'pip install C:\Users\Administrator\Downloads\numpy-1.19.2+mkl-cp38-cp38-win_amd64.whl'
+    'pip install C:\Users\Administrator\Desktop\cbm3_python-0.6.7-py3-none-any.whl'
+    'pip install ?cbm3_aws.whl'
 ]
 
 def download_file(url, local_file_path):

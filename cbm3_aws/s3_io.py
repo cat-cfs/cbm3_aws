02db5e00785ce6926a8ef8ef5939b06db8b84b54
Scott Morken <scottm361@gmail.com>
2020-10-07 15:41:31 -0700

    refactor to remove duplicated code

--- cbm3_aws/s3_io.py
+++ cbm3_aws/s3_io.py
@@ -5,6 +5,7 @@ class S3IO:
     def __init__(self, execution_s3_key_prefix, s3_interface):
         self.s3_interface = s3_interface
         self.execution_s3_key_prefix = execution_s3_key_prefix
+        self._register_methods()
 
     def _create_key_name_prefix(self, key_token):
         return "/".join([
@@ -12,42 +13,34 @@ class S3IO:
             self.execution_s3_key_prefix,
             key_token])
 
-    def download_results_database(self, project_code, simulation_id,
-                                  local_file_path):
+    def _register_methods(self):
+        self.doc_name_methods = {
+            "results": {
+                lambda **kwargs:
+                    f'{kwargs["project_code"]}_{kwargs["simulation_id"]}'
+            },
+            "tempfiles": {
+                lambda **kwargs:
+                    f'{kwargs["project_code"]}_{kwargs["simulation_id"]}'
+            },
+            "project": {
+                lambda **kwargs:
+                    f'{kwargs["project_code"]}'
+            },
+            "resource": {
+                lambda **kwargs:
+                    f'{kwargs["resource_name"]}'
+            }
+        }
+
+    def download(self, s3_key, local_path, **kwargs):
         self.s3_interface.download_compressed(
-            key_name_prefix=self._create_key_name_prefix("results"),
-            document_name=f"{project_code}_{simulation_id}",
-            local_path=local_file_path)
-
-    def upload_results_database(s3_interface, project_code, simulation_id, path):
-        s3_interface.upload_compressed(
-            "results", f"{project_code}_{simulation_id}", path)
-
-    def download_tempfiles(self, project_code, simulation_id,
-                           local_tempfiles_dir):
-        self.s3_interface.download_compressed(
-            key_name_prefix=self._create_key_name_prefix("results"),
-            document_name=f"{project_code}_tempfiles_{simulation_id}",
-            local_path=local_tempfiles_dir)
-
-    def upload_tempfiles(s3_interface, project_code, simulation_id, path):
-        s3_interface.upload_compressed(
-            "results", f"{project_code}_tempfiles_{simulation_id}", path)
-
-    def download_project_database(self, project_code, local_path):
-        self.s3_interface.download_compressed(
-            key_name_prefix=self._create_key_name_prefix("results"),
-            document_name=project_code,
+            key_name_prefix=self._create_key_name_prefix(s3_key),
+            document_name=self.doc_name_methods[s3_key](**kwargs),
             local_path=local_path)
 
-    def upload_project_database(s3_interface, project_code, path):
-        s3_interface.upload_compressed("projects", project_code, path)
-
-    def download_resource(self, name, local_path):
-        self.s3_interface.download_compressed(
-            key_name_prefix=self._create_key_name_prefix("results"),
-            document_name=name,
+    def upload(self, s3_key, local_path, **kwargs):
+        self.s3_interface.upload_compressed(
+            key_name_prefix=self._create_key_name_prefix(s3_key),
+            document_name=self.doc_name_methods[s3_key](**kwargs),
             local_path=local_path)
-
-    def upload_resource(s3_interface, name, path):
-        s3_interface.upload_compressed("resources", name, path)

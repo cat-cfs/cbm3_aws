79c22aef154f16c0edb3d812dc3b6b0566f7c4a3
Scott Morken <scottm361@gmail.com>
2020-10-08 12:35:53 -0700

    add script to entry points, new ver

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ console_scripts = [
     "cbm3_aws_instance = cbm3_aws.scripts.run_instance:main",
     "cbm3_aws_deploy = cbm3_aws.scripts.aws_deploy:main",
     "cbm3_aws_cleanup = cbm3_aws.scripts.aws_cleanup:main",
-    "cbm3_aws_start_execution = cbm3_aws.scripts.start_execution:main"
+    "cbm3_aws_start_execution = cbm3_aws.scripts.start_execution:main",
+    "cbm3_aws_s3_upload = cbm3_aws.scripts.s3_upload:main"
 ]
 
 package_data = [
@@ -22,13 +23,11 @@ package_data = [
 ]
 setup(
     name="cbm3_aws",
-    version="0.5.5",
+    version="0.5.6",
     description="Scripts for running CBM3 simulations on AWS",
     keywords=["cbm-cfs3", "AWS"],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url="",
-    download_url="",
     packages=find_packages(exclude=['test*']),
     entry_points={
         "console_scripts": console_scripts

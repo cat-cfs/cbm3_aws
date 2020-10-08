466be7ce774746e37c50c0786e37050097e1c934
Scott Morken <scottm361@gmail.com>
2020-10-08 12:32:10 -0700

    add s3_upload script

---
+++ cbm3_aws/aws/s3_upload.py
@@ -0,0 +1,47 @@
+import boto3
+import tempfile
+from cbm3_aws.s3_interface import S3Interface
+from cbm3_aws.s3_io import S3IO
+from cbm3_aws import log_helper
+
+logger = log_helper.get_logger(__name__)
+
+
+def upload(s3_bucket_name, execution_s3_key_prefix, manifest):
+    """uploads items in the specified manifest to the specified s3 bucket
+    using the execution_s3_key_prefix to produce an S3 key specific to an
+    execution name.
+
+        Example::
+
+            upload(
+                s3_bucket_name="my-bucket-name",
+                execution_s3_key_prefix="execution-id-123",
+                manifest=[
+                    {
+                        local_path: my_project_dir/project_a.mdb,
+                        s3_key="project",
+                        "project_code": "project_a"
+                    }
+                ])
+
+    Args:
+        s3_bucket_name (str): name of the AWS s3 bucket to upload items to
+        execution_s3_key_prefix (str): s3 key prefix used to make upload s3
+            keys specific to to the execution.
+        manifest (iterable): iterable of dicts with args to
+            :py:func:`cbm3_aws.s3_io.S3IO.upload`
+
+    """
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        s3_io = S3IO(
+            execution_s3_key_prefix=execution_s3_key_prefix,
+            s3_interface=S3Interface(
+                s3_resource=boto3.resource('s3'),
+                bucket_name=s3_bucket_name,
+                local_temp_dir=temp_dir))
+
+        for item in manifest:
+            logger.info(f"uploading {item}")
+            s3_io.upload(**item)
---
+++ cbm3_aws/scripts/s3_upload.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+from argparse import ArgumentParser
+from cbm3_aws import log_helper
+from cbm3_aws.aws import s3_upload
+
+
+def main():
+
+    parser = ArgumentParser(
+        description="Uploads files and dirs specified in manifest to AWS S3")
+
+    parser.add_argument(
+        "--s3_bucket_name", required=True,
+        help="name of the existing AWS s3 bucket to upload items to")
+    parser.add_argument(
+        "--execution_s3_key_prefix", required=True,
+        help="s3 key prefix used to make upload s3 keys specific to an "
+             "execution.")
+    parser.add_argument(
+        "--manifest_path", required=True, type=os.path.abspath,
+        help="s3 key prefix used to make upload s3 keys specific to an "
+             "execution.")
+
+    log_helper.start_logging("s3_upload", level="INFO")
+    logger = log_helper.get_logger("s3_upload")
+    try:
+        args = parser.parse_args()
+        logger.info("start_uploads")
+        logger.info(vars(args))
+
+        with open(args.manifest_path) as manifest_file:
+            manifest = json.load(manifest_file)
+        s3_upload.upload(
+            s3_bucket_name=args.s3_bucket_name,
+            execution_s3_key_prefix=args.execution_s3_key_prefix,
+            manifest=manifest)
+
+    except Exception:
+        logger.exception("")

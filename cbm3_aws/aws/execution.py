ca68ff8bc2cc338bb9e32e2dac2a39994ae74c8e
Scott Morken <scottm361@gmail.com>
2020-10-08 14:25:54 -0700

    finish up execution scripts and docs

--- cbm3_aws/aws/execution.py
+++ cbm3_aws/aws/execution.py
@@ -1,12 +1,27 @@
 import json
 import boto3
-from cbm3_aws.namespace import Namespace
 
 
 def start_execution(execution_name, state_machine_arn, region_name, tasks):
+    """Starts an execution on a cbm_aws cluster
+
+    Args:
+        execution_name (str): The name of the execution. This name must be
+            unique for your AWS account, region, and state machine for 90 days.
+            For more information, see Limits Related to State Machine
+            Executions in the AWS Step Functions Developer Guide.
+        state_machine_arn (str): AWS resource name for the state machine on
+            which to run the execution.
+        region_name (str): the region name
+        tasks (dict): the input data for the execution
+
+    Returns:
+        dict: the return value of the boto3 state function client
+            start_execution function.
+    """
     sfn_client = boto3.client('stepfunctions', region_name=region_name)
 
-    start_execution_response = sfn_client.start_execution(
+    return sfn_client.start_execution(
         stateMachineArn=state_machine_arn,
         name=execution_name,
-        input=json.dumps(tasks))
+        input=json.dumps({"input": tasks}))
--- cbm3_aws/scripts/start_execution.py
+++ cbm3_aws/scripts/start_execution.py
@@ -17,15 +17,19 @@ def main():
              "cbm3_aws cluster")
     parser.add_argument(
         "--execution_name", required=True,
-        help="The name of the activity to create. This name must be unique "
-             "for your AWS account and region for 90 days. For more "
-             "information, see Limits Related to State Machine Executions "
-             "in the AWS Step Functions Developer Guide.")
+        help="The name of the execution. This name must be unique for your "
+             "AWS account, region, and state machine for 90 days. For more "
+             "information, see Limits Related to State Machine Executions in "
+             "the AWS Step Functions Developer Guide.")
     parser.add_argument(
-        "--tasks_path", required=True, type=os.path.abspath,
+        "--tasks_file_path", required=True, type=os.path.abspath,
         help="Path to json formatted tasks who has at a minimum a "
              "'task_list' key whose value is the list of tasks to "
              "pass to each instance as they call get_activity_task")
+    parser.add_argument(
+        "--response_output_path", required=True, type=os.path.abspath,
+        help="Path"
+    )
 
     log_helper.start_logging("start_execution", level="INFO")
     logger = log_helper.get_logger("start_execution")
@@ -34,15 +38,26 @@ def main():
         logger.info("start_execution")
         logger.info(vars(args))
 
-        with open(args.resource_description_path, 'r') as resources_fp:
-            rd = Namespace(**json.load(resources_fp))
-        with open(args.tasks_path, 'r') as tasks_fp:
-            tasks = json.load(tasks_fp)
-        execution.start_execution(
-            execution_name=args.execution_name,
-            state_machine_arn=rd.state_machine_context.app_state_machine_arn,
-            region_name=rd.region_name,
-            tasks=tasks)
+        if os.path.exists(args.response_output_path):
+            # do not overwrite an existing file, which could potentially
+            # contain useful information.
+            raise ValueError(
+                "specified response_output_path already exists: "
+                f"'{args.response_output_path}'")
+
+        with open(args.response_output_path) as out_file:
+            with open(args.resource_description_path, 'r') as resources_fp:
+                rd = Namespace(**json.load(resources_fp))
+            with open(args.tasks_file_path, 'r') as tasks_fp:
+                tasks = json.load(tasks_fp)
+            state_machine_arn = rd.state_machine_context.app_state_machine_arn
+            start_execution_response = execution.start_execution(
+                execution_name=args.execution_name,
+                state_machine_arn=state_machine_arn,
+                region_name=rd.region_name,
+                tasks=tasks)
+            logger.info(json.dumps(start_execution_response, indent=4))
+            json.dump(start_execution_response, out_file)
 
     except Exception:
         logger.exception("")

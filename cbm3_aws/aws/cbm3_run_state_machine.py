a002320a915a71bec1005f0939674ae1b55168ec
Scott Morken <scottm361@gmail.com>
2020-10-09 18:07:56 -0700

    working on state machine

--- cbm3_aws/aws/cbm3_run_state_machine.py
+++ cbm3_aws/aws/cbm3_run_state_machine.py
@@ -10,12 +10,22 @@ def get_state_machine(task_state_machine_arn, max_concurrency):
                 "Type": "Map",
                 "ItemsPath": "$.task_list",
                 "MaxConcurrency": int(max_concurrency),
+                "Parameters": {
+                    "Input.$": "$$.Map.Item.Value"
+                },
                 "Iterator": {
                     "StartAt": "launch_cbm_task",
                     "States": {
                         "launch_cbm_task": {
                             "Type": "Task",
-                            "Resource": task_state_machine_arn,
+                            "Resource": "arn:aws:states:::states:startExecution",
+                            "Parameters": {
+                                "StateMachineArn": task_state_machine_arn,
+                                "Input": {
+                                    "Input.$": "$$.Input",
+                                    "AWS_STEP_FUNCTIONS_STARTED_BY_EXECUTION_ID.$": "$$.Execution.Id"
+                                }
+                            },
                             "End": True
                         }
                     }

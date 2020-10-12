aa3a9c8db7b0169a2ebf6dfac8cc86fdbc1b25f8
Scott Morken <scottm361@gmail.com>
2020-10-12 00:13:06 -0700

    add comma

--- cbm3_aws/scripts/run_instance.py
+++ cbm3_aws/scripts/run_instance.py
@@ -29,7 +29,7 @@ def main():
         num_workers = psutil.cpu_count()
         for _ in range(num_workers):
             args = [
-                "cbm3_aws_instance_process"
+                "cbm3_aws_instance_process",
                 "--activity_arn", args.activity_arn,
                 "--s3_bucket_name", args.s3_bucket_name,
                 "--region_name", args.region_name,
